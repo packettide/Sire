d7c6fb7080d4560e290951f32bb88072fa10c740
Max Walker <max@packettide.com>
2013-10-24 12:57:27 -0400

    Added a service provider.

---
+++ src/Packettide/Sire/SireServiceProvider.php
@@ -0,0 +1,49 @@
+<?php namespace Packettide\Sire;
+
+use Illuminate\Support\ServiceProvider;
+
+class SireServiceProvider extends ServiceProvider {
+
+        /**
+         * Indicates if loading of the provider is deferred.
+         *
+         * @var bool
+         */
+        protected $defer = false;
+
+        /**
+         * Bootstrap the application events.
+         *
+         * @return void
+         */
+        public function boot()
+        {
+                $this->package('packettide/sire');
+        }
+
+        /**
+         * Register the service provider.
+         *
+         * @return void
+         */
+        public function register()
+        {
+
+                // $this->app->booting(function()
+                // {
+                //         $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+                //         $loader->alias('Sire', 'Packettide\Sire\Sire');
+                // });
+        }
+
+        /**
+         * Get the services provided by the provider.
+         *
+         * @return array
+         */
+        public function provides()
+        {
+                return array('sire');
+        }
+
+}
